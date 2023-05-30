01e8daa1ed0434a58174556f9d5f99e3dd2dcf70
zdc <582326542@qq.com>
2023-05-30 16:41:59 +0800

    cs3

--- src/main/java/com/yeye/practiceCode/util/CodeGenerator.java
+++ src/main/java/com/yeye/practiceCode/util/CodeGenerator.java
@@ -2,12 +2,11 @@ package com.yeye.practiceCode.util;
 
 
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.OutputFile;
 import com.baomidou.mybatisplus.generator.engine.VelocityTemplateEngine;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class CodeGenerator {
 
@@ -16,10 +15,17 @@ public class CodeGenerator {
     String username = "root";
     String url = "jdbc:mysql://110.42.136.163:3306/mall_wms?useUnicode=true&characterEncoding=UTF-8&useSSL=false&serverTimezone=Asia/Shanghai";
     String finalProjectPath = System.getProperty("user.dir");
-    FastAutoGenerator.create(url, username, password)
+
+    DataSourceConfig.Builder dataSourceConfig = new DataSourceConfig.Builder(url, username, password);
+    creteModel(dataSourceConfig, finalProjectPath, Boolean.TRUE);
+//    createSingleModel(dataSourceConfig,finalProjectPath);
+  }
+
+  private static void creteModel(DataSourceConfig.Builder dataSourceConfig, String finalProjectPath, Boolean isOverride) {
+    FastAutoGenerator.create(dataSourceConfig)
       .globalConfig(builder -> {
-        builder.author("yeye") // 设置作者
-          .enableSwagger() // 开启 swagger 模式
+        builder.author("yeye1") // 设置作者
+//          .enableSwagger() // 开启 swagger 模式
           .fileOverride() // 覆盖已生成文件
           .disableOpenDir() //禁止打开输出目录
           .outputDir(finalProjectPath + "/src/main/java"); // 指定输出目录
@@ -31,7 +37,31 @@ public class CodeGenerator {
           .pathInfo(Collections.singletonMap(OutputFile.xml, finalProjectPath + "/src/main/resources/mapper")); // 设置mapperXml生成路径
 
       }).strategyConfig(builder -> {
-        builder.addTablePrefix("wms_"); // 设置过滤表前缀
+
+
+        builder.addTablePrefix("wms_");// 设置过滤表前缀
+        builder.entityBuilder() // entity配置
+          .enableRemoveIsPrefix()
+          .enableTableFieldAnnotation()
+          .enableLombok();
+
+        builder.controllerBuilder() // controller配置
+          .enableRestStyle();
+
+
+        builder.mapperBuilder() // mapper配置
+          .enableBaseResultMap()
+          .enableBaseColumnList();
+        if (isOverride) {
+          builder.entityBuilder() // entity配置
+            .fileOverride();
+          builder.controllerBuilder() // controller配置
+            .fileOverride();
+          builder.serviceBuilder() // service配置
+            .fileOverride();
+          builder.mapperBuilder() // mapper配置
+            .fileOverride();
+        }
       })
       .injectionConfig(consumer -> {
         Map<String, String> customFile = new HashMap<>();
@@ -40,8 +70,38 @@ public class CodeGenerator {
         consumer.customFile(customFile);
       }).templateEngine(new VelocityTemplateEngine())
       .execute();
+  }
+
+  private static void createSingleModel(DataSourceConfig.Builder dataSourceConfig, String finalProjectPath) {
+    FastAutoGenerator.create(dataSourceConfig)
+      // 全局配置
+      .globalConfig((scanner, builder) ->
+        builder.author(scanner.apply("请输入作者名称？"))
+          .fileOverride()
+          .outputDir(finalProjectPath + "/src/main/java"))
+      // 包配置
+      .packageConfig(builder -> {
+        builder.parent("com.yeye.practiceCode") // 设置父包名
+          .entity("model") //设置entity包名
+          .other("dao") // 设置dto包名
+          .pathInfo(Collections.singletonMap(OutputFile.xml, finalProjectPath + "/src/main/resources/mapper")); // 设置mapperXml生成路径
+
+      })
+      // 策略配置
+      .strategyConfig((scanner, builder) -> builder.addInclude(getTables(scanner.apply("请输入表名，多个英文逗号分隔？所有输入 all")))
+        .controllerBuilder().enableRestStyle()
+        .entityBuilder().enableLombok()
+        .mapperBuilder().enableBaseResultMap().enableBaseColumnList()
+        .build())
+
+      .execute();
 
 
+  }
+
+  // 处理 all 情况
+  protected static List<String> getTables(String tables) {
+    return "all".equals(tables) ? Collections.emptyList() : Arrays.asList(tables.split(","));
   }
 
 }
