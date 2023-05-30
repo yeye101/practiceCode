283aef3c323f895d89fc5321a58bb3aaefe2fd99
zdc <582326542@qq.com>
2023-05-30 17:35:05 +0800

    cs6--配置分页

---
+++ src/main/java/com/yeye/practiceCode/constant/PageConstant.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ * <p>
+ * https://www.renren.io
+ * <p>
+ * 版权所有，侵权必究！
+ */
+
+package com.yeye.practiceCode.constant;
+
+
+/**
+ * 常量
+ *
+ * @author
+ */
+public class PageConstant {
+  /**
+   * 当前页码
+   */
+  public static final String PAGE = "page";
+  /**
+   * 每页显示记录数
+   */
+  public static final String LIMIT = "limit";
+  /**
+   * 排序字段
+   */
+  public static final String ORDER_FIELD = "sidx";
+  /**
+   * 排序方式
+   */
+  public static final String ORDER = "order";
+  /**
+   * 升序
+   */
+  public static final String ASC = "asc";
+
+
+}
