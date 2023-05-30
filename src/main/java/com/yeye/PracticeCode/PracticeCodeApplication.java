6f893403a20901b4565f39a022504fd92cc1bdfe
zdc <582326542@qq.com>
2023-05-30 13:42:12 +0800

    cs1

---
+++ src/main/java/com/yeye/PracticeCode/PracticeCodeApplication.java
@@ -0,0 +1,14 @@
+package com.yeye.PracticeCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class PracticeCodeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PracticeCodeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/yeye/PracticeCode/PracticeCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeye.PracticeCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeCodeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
